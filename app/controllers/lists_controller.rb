fe8a11189db732d30c02f13af12af554b7a5c572
vagrant <vagrant@rails-dev-box>
2014-06-19 04:14:56 +0000

    Added CRUD actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,8 +8,32 @@ class ListsController < ApplicationController
   end
 
   def new
+  	@list = List.new
+  end
+
+  def create
+  	@list = List.new(params.require(:list).permit(:title, :body))
+  	if @list.save
+  		flash[:notice] = "List was saved."
+  		redirect_to @list
+  	else
+  		flash[:error] = "There was an error saving the list. Please try again."
+  		render :new
+  	end
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update_attributes(params.require(:list).permit(:title, :body))
+      flash[:notice] = "List was updated."
+      redirect_to @list
+    else
+      flash[:error] = "There was an error saving the list. Please try again."
+      render :edit
+    end
   end
 end
